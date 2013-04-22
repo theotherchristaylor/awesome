b28d6a56acd3f70122fe7b9c025011d336d04670
Chris Taylor <theotherchristaylor@gmail.com>
2013-04-22 15:23:43 -0600

    Reduced number of tags to 5.

--- rc.lua
+++ rc.lua
@@ -94,7 +94,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ "$", "www", 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag({ "$", "www", 3, 4, 5 }, s, layouts[1])
 end
 -- }}}
 
